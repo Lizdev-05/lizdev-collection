b771951ebb2de64b324b5b4905e367937e4cfbb3
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-01-31 08:50:00 +0100

    Display a single product

---
+++ src/component/Product.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router';
+import { NavLink } from 'react-router-dom';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+
+const Product = () => {
+  const { id } = useParams();
+  console.log(id);
+  const [product, setProduct] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      setLoading(true);
+      const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+
+      setProduct(await response.json());
+      setLoading(false);
+    };
+    getProducts();
+  }, []);
+
+  const Loading = () => (
+    <>
+      <div className="col-md-6">
+        <SkeletonTheme baseColor="#00A9D8" highlightColor="#f8f9f9">
+          <Skeleton height={400} />
+        </SkeletonTheme>
+      </div>
+      <div className="col-md-6" style={{ lineHeight: 2 }}>
+        <SkeletonTheme baseColor="#00A9D8" highlightColor="#f8f9f9">
+          <Skeleton height={50} width={300} />
+          <Skeleton height={75} />
+          <Skeleton height={25} width={150} />
+          <Skeleton height={50} />
+          <Skeleton height={150} />
+          <Skeleton height={50} width={100} />
+          <Skeleton height={50} width={100} style={{ marginLeft: 6 }} />
+        </SkeletonTheme>
+      </div>
+    </>
+  );
+
+  const ShowProduct = () => (
+    <>
+      <div className="col-md-6">
+        <img src={product.image} alt={product.title} height="400px" width="400px" />
+      </div>
+      <div className="col-md-6">
+        <h4 className="text-uppercase text-secondary-50">
+          {product.category}
+        </h4>
+        <h1 className="display-5">{product.title}</h1>
+        <p className="lead fw-bolder">
+
+          Rating:
+
+          {product.rating && product.rating.rate}
+          <span>⭐</span>
+        </p>
+        <h3 className="display-6 fw-bold my-4">
+          $
+          {product.price}
+        </h3>
+        <p className="lead">
+          {product.description}
+        </p>
+        <button type="button" className="btn btn-outline-primary px-4 py-2">Add to Cart</button>
+        {/* <NavLink className="btn btn-outline-primary ms-2 px-3 py-2">
+          Go to Cart
+        </NavLink> */}
+      </div>
+    </>
+  );
+
+  return (
+    <div className="container py-5">
+      <div className="row py-5">
+        {loading ? <Loading /> : <ShowProduct />}
+      </div>
+    </div>
+  );
+};
+export default Product;
