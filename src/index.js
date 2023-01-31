1b7fe96cc88032f162b792ed24fd7700d583f139
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-01-31 14:05:11 +0100

    Add store to the index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 // import '../node_modules/font-awesome/css/font-awesome.min.css';
+import { Provider } from 'react-redux';
+import store from './redux/store/Index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Provider store={store}> 
       <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );
