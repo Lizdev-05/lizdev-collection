d955a5496dc13408be0c387e4da0221a2f896121
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-01-31 08:46:30 +0100

    Add products routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './component/header/Header';
+import Products from './component/Products';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Products />} />
+        
+      </Routes>
     </div>
   );
 }
