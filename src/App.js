93ae34613b7b68d15912398c302e08e1c906bcc3
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-01-31 08:46:50 +0100

    Add product routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './component/header/Header';
 import Products from './component/Products';
+import Products from './component/Product';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Products />} />
-        
+        <Route path="/products/:id" element={<Product />} />
       </Routes>
     </div>
   );
