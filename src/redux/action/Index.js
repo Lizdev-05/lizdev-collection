e7b6887289bfa6e87b88bd8737db8157b74286fd
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-02-01 10:07:31 +0100

    update the impport

--- src/redux/action/Index.js
+++ src/redux/action/Index.js
@@ -1,19 +1,8 @@
-// export const addCart = (product) => ({
-// type: 'ADD_ITEM_TO_CART',
-// payload: product,
-// });
-
-// export const delCart = (product) => ({
-// type: 'DELETE_ITEM',
-// payload: product,
-// });
-
 export const addCart = (product) => ({
   type: 'ADD_ITEM_TO_CART',
   payload: product,
 });
 
-// For deleting item from the Cart
 export const delCart = (product) => ({
   type: 'DELETE_ITEM',
   payload: product,
