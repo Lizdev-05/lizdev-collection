2a4e86959010604aecb9936ce51cfc10b3b94d27
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-02-01 18:45:27 +0100

    Add onclick functiong using usestate

--- src/component/Checkout.js
+++ src/component/Checkout.js
@@ -1,231 +1,32 @@
 /* eslint-disable */
 import React from "react";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
 const Checkout = () => {
   const cart = useSelector((state) => state.handleCart);
 
   const price = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+
+  const publicKey = "pk_your_public_key_here";
+  const amount = 1000000; // Remember, set in kobo!
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phone, setPhone] = useState("");
+
   return (
-    // <div className="row g-5">
-    //   <div className="col-md-5 col-lg-4 order-md-last">
-    //     <h4 className="d-flex justify-content-between align-items-center mb-3">
-    //       <span className="text-primary">Your cart</span>
-    //       <span className="badge bg-primary rounded-pill">{cart.length}</span>
-    //     </h4>
-    //     <table className="table">
-    //       <thead>
-    //         <tr>
-    //           <th scope="col">Product</th>
-    //           <th scope="col">Description</th>
-    //           <th scope="col">Quantity</th>
-    //           <th scope="col">Price</th>
-    //         </tr>
-    //       </thead>
-    //       <tbody>
-    //         {cart.map((product) => (
-    //           <tr key={product.id}>
-    //             <th className="col-md-6">
-    //               <img
-    //                 src={product.image}
-    //                 alt={product.title}
-    //                 height="200px"
-    //                 width="200px"
-    //               />
-    //             </th>
-    //             <td>{product.title}</td>
-    //             <td>{product.quantity}</td>
-    //             <td>${product.price}</td>
-    //           </tr>
-    //         ))}
-    //         <tr>
-    //           <td>Total (USD)</td>
-    //           <td>{price}</td>
-    //         </tr>
-    //       </tbody>
-    //     </table>
-
-    //     <div className="col-md-7 col-lg-8">
-    //       <h4 className="mb-3">Billing address</h4>
-    //       {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-    //   <form action="" className="needs-validation" noValidate>
-    //     <div className="row g-3">
-    //       <div className="col-sm-6">
-    //         <label htmlFor="firstName" className="form-label">
-    //           First name
-    //         </label>
-    //         <input
-    //           type="text"
-    //           className="form-control"
-    //           id="firstName"
-    //           placeholder=""
-    //           value=""
-    //           required
-    //         />
-    //         <div className="invalid-feedback">
-    //           Valid first name is required.
-    //         </div>
-    //       </div>
-
-    //       <div className="col-sm-6">
-    //         <label htmlFor="lastName" className="form-label">
-    //           Last name
-    //         </label>
-    //         <input
-    //           type="text"
-    //           className="form-control"
-    //           id="lastName"
-    //           placeholder=""
-    //           value=""
-    //           required
-    //         />
-    //         <div className="invalid-feedback">
-    //           Valid last name is required.
-    //         </div>
-    //       </div>
-    //       <div className="col-12">
-    //         <label htmlFor="email" className="form-label">
-    //           Email{" "}
-    //         </label>
-    //         <input
-    //           type="email"
-    //           className="form-control"
-    //           id="email"
-    //           placeholder="you@example.com"
-    //         />
-    //         <div className="invalid-feedback">
-    //           Please enter a valid email address for shipping updates.
-    //         </div>
-    //       </div>
-    //     </div>
-
-    //     <hr className="my-4" />
-
-    //     <h4 className="mb-3">Payment</h4>
-
-    //     <div className="my-3">
-    //       <div className="form-check">
-    //         <input
-    //           id="credit"
-    //           name="paymentMethod"
-    //           type="radio"
-    //           className="form-check-input"
-    //           checked
-    //           required
-    //         />
-    //         <label className="form-check-label" htmlFor="credit">
-    //           Credit card
-    //         </label>
-    //       </div>
-    //       <div className="form-check">
-    //         <input
-    //           id="debit"
-    //           name="paymentMethod"
-    //           type="radio"
-    //           className="form-check-input"
-    //           required
-    //         />
-    //         <label className="form-check-label" htmlFor="debit">
-    //           Debit card
-    //         </label>
-    //       </div>
-    //       <div className="form-check">
-    //         <input
-    //           id="paypal"
-    //           name="paymentMethod"
-    //           type="radio"
-    //           className="form-check-input"
-    //           required
-    //         />
-    //         <label className="form-check-label" htmlFor="paypal">
-    //           PayPal
-    //         </label>
-    //       </div>
-    //     </div>
-
-    //     <div className="row gy-3">
-    //       <div className="col-md-6">
-    //         <label htmlFor="cc-name" className="form-label">
-    //           Name on card
-    //         </label>
-    //         <input
-    //           type="text"
-    //           className="form-control"
-    //           id="cc-name"
-    //           placeholder=""
-    //           required
-    //         />
-    //         <small className="text-muted">
-    //           Full name as displayed on card
-    //         </small>
-    //         <div className="invalid-feedback">Name on card is required</div>
-    //       </div>
-
-    //       <div className="col-md-6">
-    //         <label htmlFor="cc-number" className="form-label">
-    //           Credit card number
-    //         </label>
-    //         <input
-    //           type="text"
-    //           className="form-control"
-    //           id="cc-number"
-    //           placeholder=""
-    //           required
-    //         />
-    //         <div className="invalid-feedback">
-    //           Credit card number is required
-    //         </div>
-    //       </div>
-
-    //       <div className="col-md-3">
-    //         <label htmlFor="cc-expiration" className="form-label">
-    //           Expiration
-    //         </label>
-    //         <input
-    //           type="text"
-    //           className="form-control"
-    //           id="cc-expiration"
-    //           placeholder=""
-    //           required
-    //         />
-    //         <div className="invalid-feedback">Expiration date required</div>
-    //       </div>
-
-    //       <div className="col-md-3">
-    //         <label htmlFor="cc-cvv" className="form-label">
-    //           CVV
-    //         </label>
-    //         <input
-    //           type="text"
-    //           className="form-control"
-    //           id="cc-cvv"
-    //           placeholder=""
-    //           required
-    //         />
-    //         <div className="invalid-feedback">Security code required</div>
-    //       </div>
-    //     </div>
-
-    //     <hr className="my-4" />
-
-    //     <button className="w-100 btn btn-primary btn-lg" type="submit">
-    //       Continue to checkout
-    //     </button>
-    //   </form>
-    //     </div>
-    //   </div>
-
-    <div class="container">
+    <div className="container">
       <main>
-        <div class="row g-5">
-          <div class="col-md-5 col-lg-6 order-md-last">
-            <h4 class="d-flex justify-content-between align-items-center mb-3">
-              <span class="text-primary">Your cart</span>
-              <span class="badge bg-primary rounded-pill">3</span>
+        <div className="row g-5">
+          <div className="col-md-5 col-lg-6 order-md-last">
+            <h4 className="d-flex justify-content-between align-items-center mb-3">
+              <span className="text-primary">Your cart</span>
+              <span className="badge bg-primary rounded-pill">3</span>
             </h4>
 
-            <ul class="list-group mb-3">
-              <li class="list-group-item d-flex justify-content-between lh-sm">
+            <ul className="list-group mb-3">
+              <li className="list-group-item d-flex justify-content-between lh-sm">
                 <table className="table">
                   <thead>
                     <tr>
@@ -260,8 +61,8 @@ const Checkout = () => {
               </li>
             </ul>
           </div>
-          <div class="col-md-7 col-lg-6">
-            <h4 class="mb-3">Billing address</h4>
+          <div className="col-md-7 col-lg-6">
+            <h4 className="mb-3">Billing address</h4>
             <form action="" className="needs-validation" noValidate>
               <div className="row g-3">
                 <div className="col-sm-6">
@@ -275,6 +76,7 @@ const Checkout = () => {
                     placeholder=""
                     value=""
                     required
+                    onChange={(e) => setFirstName(e.target.value)}
                   />
                   <div className="invalid-feedback">
                     Valid first name is required.
@@ -292,6 +94,7 @@ const Checkout = () => {
                     placeholder=""
                     value=""
                     required
+                    onChange={(e) => setLastName(e.target.value)}
                   />
                   <div className="invalid-feedback">
                     Valid last name is required.
@@ -306,6 +109,7 @@ const Checkout = () => {
                     className="form-control"
                     id="email"
                     placeholder="you@example.com"
+                    onChange={(e) => setEmail(e.target.value)}
                   />
                   <div className="invalid-feedback">
                     Please enter a valid email address for shipping updates.
@@ -425,10 +229,13 @@ const Checkout = () => {
               </div>
 
               <hr className="my-4" />
-
-              <button className="w-100 btn btn-primary btn-lg" type="submit">
+              <PaystackButton
+                className="paystack-button w-100 btn btn-primary btn-lg"
+                {...componentProps}
+              />
+              {/* <button className="w-100 btn btn-primary btn-lg" type="submit">
                 Continue to checkout
-              </button>
+              </button> */}
             </form>
           </div>
         </div>
